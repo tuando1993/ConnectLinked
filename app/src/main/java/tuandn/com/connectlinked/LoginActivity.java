0df91f8e976c1bc2195417cce9a3675355a1e379
TuanDN2 <kingdragon102@gmail.com>
2015-06-29 11:11:20 +0700

    Login Function

--- app/src/main/java/tuandn/com/connectlinked/LoginActivity.java
+++ app/src/main/java/tuandn/com/connectlinked/LoginActivity.java
@@ -4,17 +4,12 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Toast;
 
-import com.linkedin.platform.APIHelper;
 import com.linkedin.platform.AccessToken;
 import com.linkedin.platform.LISession;
 import com.linkedin.platform.LISessionManager;
-import com.linkedin.platform.errors.LIApiError;
 import com.linkedin.platform.errors.LIAuthError;
-import com.linkedin.platform.listeners.ApiListener;
-import com.linkedin.platform.listeners.ApiResponse;
 import com.linkedin.platform.listeners.AuthListener;
 import com.linkedin.platform.utils.Scope;
 
@@ -32,7 +27,8 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen);
 
-        Auth();
+        Login();
+
     }
 
     // Build the list of member permissions our LinkedIn session requires
@@ -40,29 +36,44 @@ public class LoginActivity extends Activity {
         return Scope.build(Scope.R_BASICPROFILE, Scope.W_SHARE);
     }
 
-    private void Auth(){
-        List<String> scope = new ArrayList<String>();
-        scope.add("r_basicprofile");
-        scope.add("w_share");
+    private void Login(){
 
-        LISessionManager.getInstance(getApplicationContext()).init(LoginActivity.this, buildScope(), new AuthListener() {
-            @Override
-            public void onAuthSuccess() {
-                // Authentication was successful.  You can now do
-                // other calls with the SDK.
-                Toast.makeText(getApplication(), "Success", Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void onAuthError(LIAuthError error) {
-                Toast.makeText(getApplication(), error.toString(), Toast.LENGTH_LONG).show();
-                System.out.println(error.toString());
-            }
-        }, true);
 
         LISession liSession = LISessionManager.getInstance(LoginActivity.this).getSession();
-        AccessToken accessToken = liSession.getAccessToken();
-//        Toast.makeText(getApplication(), accessToken.toString(), Toast.LENGTH_LONG).show();
+        AccessToken accessToken= liSession.getAccessToken();
+
+        if (accessToken != null) {
+//            Start Main Activity
+            Intent i = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(i);
+            finish();
+        }
+            else{
+
+            //Add Permissions
+            List<String> scope = new ArrayList<String>();
+            scope.add("r_basicprofile");
+            scope.add("w_share");
+
+            LISessionManager.getInstance(getApplicationContext()).init(LoginActivity.this, buildScope(), new AuthListener() {
+                @Override
+                public void onAuthSuccess() {
+                    // Authentication was successful.  You can now do
+                    // other calls with the SDK.
+                    Toast.makeText(getApplication(), "Success", Toast.LENGTH_LONG).show();
+                }
+
+                @Override
+                public void onAuthError(LIAuthError error) {
+                    Toast.makeText(getApplication(), error.toString(), Toast.LENGTH_LONG).show();
+                    System.out.println(error.toString());
+                }
+            }, true);
+//                    Start Main Activity
+                    Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(i);
+                    finish();
+                }
     }
 
 //        String url = "https://api.linkedin.com/v1/people/~:(id,first-name,last-name)";
@@ -79,19 +90,7 @@ public class LoginActivity extends Activity {
 //                Toast.makeText(getApplication(),"Error",Toast.LENGTH_LONG).show();
 //            }
 //        });
-//        Check if another already logged in
-//        if (false) {
-////            Start Main Activity
-//            Intent i = new Intent(LoginActivity.this, MainActivity.class);
-//            startActivity(i);
-//            finish();
-//        }
-//            else{
-////                    Start Main Activity
-//                    Intent i = new Intent(LoginActivity.this, MainActivity.class);
-//                    startActivity(i);
-//                    finish();
-//                }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
--- app/src/main/java/tuandn/com/connectlinked/MainActivity.java
+++ app/src/main/java/tuandn/com/connectlinked/MainActivity.java
@@ -8,7 +8,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.linkedin.platform.APIHelper;
+import com.linkedin.platform.LISession;
+import com.linkedin.platform.LISessionManager;
+import com.linkedin.platform.errors.LIApiError;
+import com.linkedin.platform.listeners.ApiListener;
+import com.linkedin.platform.listeners.ApiResponse;
+
+import org.json.JSONObject;
 
 import Utils.Utils;
 
@@ -18,7 +28,7 @@ import Utils.Utils;
 public class MainActivity extends Activity {
     private Button post;
     private SharedPreferences mSharedPreferences;
-    private EditText username;
+    private TextView username;
     private Context mContext;
 
 
@@ -27,6 +37,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_screen);
         mContext = MainActivity.this;
+        username = (TextView) findViewById(R.id.user_name);
 
         post = (Button) findViewById(R.id.post);
 
@@ -34,6 +45,29 @@ public class MainActivity extends Activity {
     }
 
     private void displayUsername() {
+        LISession liSession = LISessionManager.getInstance(MainActivity.this).getSession();
+
+        String url = "https://api.linkedin.com/v1/people/~:(id,first-name,last-name)";
+
+        APIHelper apiHelper = APIHelper.getInstance(getApplicationContext());
+        apiHelper.getRequest(this, url, new ApiListener() {
+            @Override
+            public void onApiSuccess(ApiResponse apiResponse) {
+                System.out.println(apiResponse.toString());
+                try{
+                    JSONObject object = apiResponse.getResponseDataAsJson();
+                    username.setText("Hello, "+object.getString("firstName") +" "+ object.getString("lastName"));
+                }
+                catch (Exception e){
+
+                }
+            }
+
+            @Override
+            public void onApiError(LIApiError LIApiError) {
+                Toast.makeText(getApplication(),"Error",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     public void postOnWall(View v){
